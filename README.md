a79acf3ccea099bcd83a28bf1806312dae82fc95
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-12-10 03:09:17 -0300

    Initial commit 🎉

---
+++ README.md
@@ -0,0 +1,12 @@
+# Yummer
+
+Site de pedidos de cookies
+
+### Tech
+
+- [Next.js](next)
+- Typescript
+- [Sass](sass)
+
+[next]: https://nextjs.org/
+[sass]: https://sass-lang.com/
---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,800&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
