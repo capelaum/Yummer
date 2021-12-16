fe9979a711dd7293c29f7af5e5eb3991d5e4fb4a
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-12-16 15:45:32 -0300

    refactor: Cart Footer

--- src/styles/cart.ts
+++ src/styles/cart.ts
@@ -27,7 +27,7 @@ export const Checkout = styled.section`
   background-size: 250px;
 `;
 
-export const CheckoutContainer = styled.div`
+export const CartContainer = styled.div`
   width: 90%;
   max-width: 1200px;
   margin: 0 auto 4rem;
@@ -42,79 +42,7 @@ export const CheckoutContainer = styled.div`
 
   border-radius: 20px;
 
-  footer {
-    width: 100%;
-    margin-top: 2rem;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-
-    button {
-      padding: 1rem 4rem;
-      border-radius: 50px;
-
-      text-transform: uppercase;
-
-      font-size: 1.5rem;
-      font-weight: 500;
-
-      color: white;
-      background-color: var(--color-secondary);
-
-      transition: all 0.3s;
-
-      &:hover {
-        scale: 1.05;
-      }
-    }
-  }
-
   @media (max-width: 768px) {
     padding: 1rem;
-
-    footer {
-      margin-top: 0;
-      flex-direction: column-reverse;
-
-      button {
-        padding: 1rem;
-        font-size: 1.25rem;
-
-        width: 100%;
-      }
-    }
-  }
-`;
-
-export const Total = styled.div`
-  display: flex;
-  align-items: flex-end;
-
-  span {
-    font-size: 1rem;
-    color: var(--color-secondary);
-    font-weight: 600;
-    margin-bottom: 0.2rem;
-  }
-
-  .checkout_total {
-    padding-left: 0.4rem;
-    font-size: 2rem;
-    color: #fff;
-    font-weight: 500;
-  }
-
-  @media (max-width: 768px) {
-    width: 100%;
-    flex-direction: column;
-    align-items: flex-start;
-
-    margin: 1.5rem 0;
-
-    .checkout_total {
-      padding-left: 0;
-      font-size: 2rem;
-    }
   }
 `;
