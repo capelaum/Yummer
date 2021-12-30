7320b50b65e49123186fb0c3fbb7cea28390991a
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-12-30 19:34:26 -0300

    update: slider

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -22,4 +22,25 @@ export const testimonials: Testimonial[] = [
     nick: "acyrino_",
     link: "https://www.instagram.com/acyrino_",
   },
+  {
+    text: "Só de olhar da vontade de enfiar tudo na boca, e ao experimentar, foi isso mesmo que fiz, peguei um copo de leite para diluir e enfiei tudo na boca hihihi",
+    author: "Gabriel Antero",
+    imageSrc: "vacaum.jpg",
+    nick: "gabriel.antero",
+    link: "https://www.instagram.com/gabriel.antero",
+  },
+  {
+    text: "Mano, é muito bom! Apesar de nem poder comer.. Recomendo aa todos amantes da culinária de sobremesas, como o Vacaum.",
+    author: "Hugo S Correia",
+    imageSrc: "daya.jpg",
+    nick: "h_correia",
+    link: "https://www.instagram.com/h_correia",
+  },
+  {
+    text: "To ficando gordinho já de tanto comer essas delícias.. mas realmente não tem como, é um sacrifício que estou disposto a fazer.",
+    author: "Adriano Cyrino",
+    imageSrc: "cica.jpg",
+    nick: "acyrino_",
+    link: "https://www.instagram.com/acyrino_",
+  },
 ];
